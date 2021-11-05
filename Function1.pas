43feefe7ba01e939bc8834f3a3b0cbe60fb46506
John Moshakis <john.moshakis@firmex.com>
2021-11-04 22:37:01 -0400

    Sample

---
+++ Function1.pas
@@ -0,0 +1,32 @@
+﻿namespace FunctionApp3;
+
+uses
+  Microsoft.Azure.Functions.Worker,
+  Microsoft.Azure.Functions.Worker.Http,
+  System.Net;
+
+type
+  Function1 = public static class
+  private
+  protected
+  public
+    [&Function('Function1')]
+    class method Run([HttpTrigger(AuthorizationLevel.Function, 'get', 'post')]request:HttpRequestData;
+      executionContext:FunctionContext):Task<HttpResponseData>;
+    begin
+
+      var logger := executionContext.GetLogger('Function1');
+      logger.LogInformation('Oxygene HTTP trigger function processed a request.');
+
+      var response := request.CreateResponse(HttpStatusCode.OK);
+
+      response.Headers.Add('Content-Type', 'text/plain; charset=utf-8');
+      response.WriteString('Welcome to Azure Functions!');
+
+      exit Task.FromResult(response);
+
+    end;
+  end;
+
+
+end.
---
+++ Program.pas
@@ -0,0 +1,22 @@
+﻿namespace FunctionApp3;
+
+uses
+  Microsoft.Extensions.Hosting,
+  System.Linq;
+
+type
+  Program = class
+  public
+    class method Main(args: array of String): Task<Int32>;
+    begin
+      var host := new HostBuilder()
+          .ConfigureFunctionsWorkerDefaults()
+          .Build();
+
+      await host.RunAsync;
+    end;
+
+  end;
+
+
+end.
